33d34c5ba523409ab3ca74a73b3eaaa82f40a6d3
Derek Kniffin <oddityoverseer13@gmail.com>
2015-04-13 21:04:24 -0600

    Cleaned up test file

--- test.rb
+++ test.rb
@@ -1,16 +1,6 @@
 require_relative 'board.rb'
 require_relative 'ai.rb'
 
-# puts ai.setHeuristic(['X','X','X']) # 100
-# puts ai.setHeuristic(['O','O','O']) # -100
-# puts ai.setHeuristic(['X','X',nil]) # 10
-# puts ai.setHeuristic(['O','O',nil]) # -10
-# puts ai.setHeuristic(['X',nil,nil]) # 1
-# puts ai.setHeuristic(['O',nil,nil]) # -1
-
-### Test cases ###
-# 1. if two in row for self, then win
-
 def test(ai,testNum,testName,a)
 	b = Board.new(3,3,a)
 	puts "Test #{testNum}: #{testName}"
@@ -22,61 +12,61 @@ def test(ai,testNum,testName,a)
 end
 
 
-# test(ai, 1.1, "finish row to win",
-# 	[['X','X',nil],
-# 	 [nil,nil,nil],
-# 	 [nil,nil,nil]])
-# test(ai, 1.2, "finish row to win",
-# 	[[nil,nil,nil],
-# 	 [nil,'X','X'],
-# 	 [nil,nil,nil]])
-# test(ai, 1.3, "finish row to win",
-# 	[[nil,nil,nil],
-# 	 [nil,nil,nil],
-# 	 ['X',nil,'X']])
+test(ai, 1.1, "finish row to win",
+	[['X','X',nil],
+	 [nil,nil,nil],
+	 [nil,nil,nil]])
+test(ai, 1.2, "finish row to win",
+	[[nil,nil,nil],
+	 [nil,'X','X'],
+	 [nil,nil,nil]])
+test(ai, 1.3, "finish row to win",
+	[[nil,nil,nil],
+	 [nil,nil,nil],
+	 ['X',nil,'X']])
 
-# test(ai, 2.1, "finish col to win",
-# 	[['X',nil,nil],
-# 	 ['X',nil,nil],
-# 	 [nil,nil,nil]])
-# test(ai, 2.2, "finish col to win",
-# 	[[nil,nil,nil],
-# 	 [nil,'X',nil],
-# 	 [nil,'X',nil]])
-# test(ai, 2.3, "finish col to win",
-# 	[[nil,nil,'X'],
-# 	 [nil,nil,nil],
-# 	 [nil,nil,'X']])
+test(ai, 2.1, "finish col to win",
+	[['X',nil,nil],
+	 ['X',nil,nil],
+	 [nil,nil,nil]])
+test(ai, 2.2, "finish col to win",
+	[[nil,nil,nil],
+	 [nil,'X',nil],
+	 [nil,'X',nil]])
+test(ai, 2.3, "finish col to win",
+	[[nil,nil,'X'],
+	 [nil,nil,nil],
+	 [nil,nil,'X']])
 
-# test(ai, 3.1, "finish diag to win",
-# 	[[nil,nil,nil],
-# 	 [nil,'X',nil],
-# 	 [nil,nil,'X']])
-# test(ai, 3.2, "finish diag to win",
-# 	[[nil,nil,'X'],
-# 	 [nil,'X',nil],
-# 	 [nil,nil,nil]])
+test(ai, 3.1, "finish diag to win",
+	[[nil,nil,nil],
+	 [nil,'X',nil],
+	 [nil,nil,'X']])
+test(ai, 3.2, "finish diag to win",
+	[[nil,nil,'X'],
+	 [nil,'X',nil],
+	 [nil,nil,nil]])
 
-# test(ai, 4.1, "block opponent if they have two",
-# 	[['O','O',nil],
-# 	 [nil,nil,nil],
-# 	 [nil,nil,nil]])
-# test(ai, 4.2, "block opponent if they have two",
-# 	[[nil,'O','X'],
-# 	 [nil,'O',nil],
-# 	 [nil,nil,nil]])
+test(ai, 4.1, "block opponent if they have two",
+	[['O','O',nil],
+	 [nil,nil,nil],
+	 [nil,nil,nil]])
+test(ai, 4.2, "block opponent if they have two",
+	[[nil,'O','X'],
+	 [nil,'O',nil],
+	 [nil,nil,nil]])
 
-# test(ai, 5.1, "win, even if opponent has two",
-# 	[['X','O',nil],
-# 	 ['X','O',nil],
-# 	 [nil,nil,nil]])
+test(ai, 5.1, "win, even if opponent has two",
+	[['X','O',nil],
+	 ['X','O',nil],
+	 [nil,nil,nil]])
 
-# test(ai, 6.1, "get two if we can",
-# 	[['O',nil,'X'],
-# 	 [nil,nil,nil],
-# 	 [nil,nil,nil]])
-# test(ai, 6.2, "get two if we can",
-# 	[[nil,nil,'X'],
-# 	 [nil,'O',nil],
-# 	 [nil,nil,nil]])
+test(ai, 6.1, "get two if we can",
+	[['O',nil,'X'],
+	 [nil,nil,nil],
+	 [nil,nil,nil]])
+test(ai, 6.2, "get two if we can",
+	[[nil,nil,'X'],
+	 [nil,'O',nil],
+	 [nil,nil,nil]])
 
