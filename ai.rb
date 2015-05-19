d6ef46868bd3ef16030bc7eb5d9e23ae1018fa55
Derek Kniffin <oddityoverseer13@gmail.com>
2015-05-19 08:04:03 -0600

    Fixed another bug in the AI

--- ai.rb
+++ ai.rb
@@ -57,6 +57,8 @@ class AI
 			-10
 		elsif set[1] == @player # Owning the center space
 			5
+		elsif set[0] == @player || set[2] == @player # Owning a corner space
+			3
 		elsif myCount == 1 # Owning another space
 			1
 		elsif oppCount == 1
